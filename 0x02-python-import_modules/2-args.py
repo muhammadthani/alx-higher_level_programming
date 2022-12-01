d3e7b96111aa2dc8203d150090b46338c00863d5
muhammadthani <thaniabdulkadir@gmail.com>
2022-12-01 10:56:54 -0800

    2-args.py4

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) == 1:
-    print('{:d} arguments.'.format(len(sys.argv) - 1))
-elif len(sys.argv) == 2:
-    print('{:d} argument:'.format((len(sys.argv) - 1)))
-else:
-    print('{:d} arguments:'.format(len(sys.argv) - 1))
-for i in range(1, (len(sys.argv))):
-    print('{:d}: {:s}'.format(i, (sys.argv[i])))
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print('{:d} arguments.'.format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print('{:d} argument:'.format((len(sys.argv) - 1)))
+    else:
+        print('{:d} arguments:'.format(len(sys.argv) - 1))
+    for i in range(1, (len(sys.argv))):
+        print('{:d}: {:s}'.format(i, (sys.argv[i])))
