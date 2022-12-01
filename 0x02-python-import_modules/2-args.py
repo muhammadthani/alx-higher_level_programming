c56551fb77c2ff3cde07ac8ac88edddca98cda14
muhammadthani <thaniabdulkadir@gmail.com>
2022-12-01 10:18:42 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print('{:d} arguments.'.format(len(sys.argv) - 1))
+else:
+    print('{:d} arguments:'.format(len(sys.argv)))
+    for i in range(1, (len(sys.argv))):
+        print('{:d}: {:s}'.format(i, (sys.argv[i])))
