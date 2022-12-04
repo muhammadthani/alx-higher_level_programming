380cb878e309fea7274a9072b788c1afcdbc44dc
muhammadthani <thaniabdulkadir@gmail.com>
2022-12-04 15:02:27 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == '':
-        sentence[0] = None
-    elif sentence != '':
+    if sentence != '':
         initial_char = sentence[0]
+    else:
+        initial_char = None
     return (len(sentence), initial_char)
