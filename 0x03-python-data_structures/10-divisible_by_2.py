e482c0a010e21da4235878672cfc089c549a5ba1
muhammadthani <thaniabdulkadir@gmail.com>
2022-12-05 12:07:18 -0800

    10-divisible_by_2.pytrial

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = my_list.copy()
+    len(new_list) == len(my_list)
     for a in new_list:
         if a % 2 == 0:
             new_list[a] = True
